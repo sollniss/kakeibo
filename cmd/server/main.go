e3193f22c6b71cca284dc3a9220a3c2a6921d223
sollniss <sollniss@web.de>
2023-09-30 18:08:12 +0900

    add server main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/sollniss/graceful"
+	"github.com/sollniss/kakeibo/repository/xsyncmap"
+	"github.com/sollniss/kakeibo/transport/http/html/plain"
+	"github.com/sollniss/kakeibo/transport/http/router/httprouter"
+	"github.com/sollniss/kakeibo/usecases/transfer"
+)
+
+func main() {
+	transferRepository := xsyncmap.NewTransferRepository()
+	transferRepository.AddDummyData()
+
+	transferUsecase := transfer.NewUsecase(nil, transferRepository, transferRepository)
+	frontend := plain.NewFrontend()
+
+	router := httprouter.NewRouter(transferUsecase, frontend)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":8080",
+	}
+
+	ctx := graceful.NotifyShutdown()
+	err := graceful.ListenAndServe(ctx, server, 60*time.Second)
+	if err != nil {
+		log.Printf("error during shutdown: %s", err)
+		return
+	}
+	log.Print("gracefully shut down")
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/sollniss/resperr v0.0.0-20230424140111-7d502219de2d
 require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/puzpuzpuz/xsync/v2 v2.5.1
+	github.com/sollniss/graceful v0.0.0-20230924070016-c29142d29890
 )
